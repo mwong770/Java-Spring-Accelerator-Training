577eb4d1f4c3cec53667c1563fd57e80fcc23ded
Maria Wong <maria@Marias-MacBook-Pro.local>
2019-06-03 17:53:54 -0400

    adds june 4 hw

---
+++ 2019_06_04/U1M1L1Katas/src/main/java/com/company/AChattyProgram.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class AChattyProgram {
+    public static void main(String[] args) {
+        System.out.println( "Hi!\n" +
+                            "How are you?\n" +
+                            "I'm fine!\n" +
+                            "Doing really well in fact...\n" +
+                            "Isn't it a beautiful day?\n" +
+                            "Smiling is my favorite\n" +
+                            "Good bye!!" );
+    }
+}
