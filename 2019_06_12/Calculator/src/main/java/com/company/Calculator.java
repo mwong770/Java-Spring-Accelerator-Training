ea929ab99d750d5795162ed73e52ac8897b7338b
Maria Wong <maria@Marias-MacBook-Pro.local>
2019-06-12 08:25:08 -0400

    adds static keyword to june 12 hw

--- 2019_06_12/Calculator/src/main/java/com/company/Calculator.java
+++ 2019_06_12/Calculator/src/main/java/com/company/Calculator.java
@@ -9,19 +9,19 @@ package com.company;
 
 public class Calculator {
 
-    public int add(int x, int y) {
+    public static int add(int x, int y) {
         return x + y;
     }
 
-    public int subtract(int x, int y) {
+    public static int subtract(int x, int y) {
         return x-y;
     }
 
-    public int multiply(int x, int y) {
+    public static int multiply(int x, int y) {
         return x*y;
     }
 
-    public int divide(int x, int y) {
+    public static int divide(int x, int y) {
         return x/y;
     }
 
--- 2019_06_12/CalculatorRefactored/Calculator/src/main/java/com/company/Calculator.java
+++ 2019_06_12/CalculatorRefactored/Calculator/src/main/java/com/company/Calculator.java
@@ -12,73 +12,73 @@ public class Calculator {
 
     // int parameters
 
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    public static int subtract(int a, int b) {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    public static int multiply(int a, int b) {
         return a * b;
     }
 
-    public int divide(int a, int b) {
+    public static int divide(int a, int b) {
         return a / b;
     }
 
     // long parameters
 
-    public long add(long a, long b) {
+    public static long add(long a, long b) {
         return a + b;
     }
 
-    public long subtract(long a, long b) {
+    public static long subtract(long a, long b) {
         return a - b;
     }
 
-    public long multiply(long a, long b) {
+    public static long multiply(long a, long b) {
         return a * b;
     }
 
-    public long divide(long a, long b) {
+    public static long divide(long a, long b) {
         return a / b;
     }
 
     // float parameters
 
-    public float add(float a, float b) {
+    public static float add(float a, float b) {
         return a + b;
     }
 
-    public float subtract(float a, float b) {
+    public static float subtract(float a, float b) {
         return a - b;
     }
 
-    public float multiply(float a, float b) {
+    public static float multiply(float a, float b) {
         return a * b;
     }
 
-    public float divide(float a, float b) {
+    public static float divide(float a, float b) {
         return a / b;
     }
 
     // double parameters
 
-    public double add(double a, double b) {
+    public static double add(double a, double b) {
         return a + b;
     }
 
-    public double subtract(double a, double b) {
+    public static double subtract(double a, double b) {
         return a - b;
     }
 
-    public double multiply(double a, double b) {
+    public static double multiply(double a, double b) {
         return a * b;
     }
 
-    public double divide(double a, double b) {
+    public static double divide(double a, double b) {
         return a / b;
     }
 
